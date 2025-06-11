71797eada440046c4b94645d32fe5b76d1e8d6e8
Riley Loo <shaunloo10@gmail.com>
2025-06-10 20:59:05 -0500

    cart: complete PNG decoding logic
    Signed-off-by: Riley Loo <shaunloo10@gmail.com>

--- src/cart.c
+++ src/cart.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdint.h>
 #include <zlib.h>
+#include <stdio.h>
 
 #include "serial.h"
 #include "cart.h"
@@ -15,6 +16,7 @@ int fd; // Globals are like, fine for this purpose. Bleh.
 PNG_Chunk ihdr;
 PNG_Chunk idat;
 uint8_t *decompressed_data;
+pix_t *fb;
 
 static const unsigned char PNG_SIG[8] = {
     0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A
@@ -113,23 +115,111 @@ int scan_cart() {
     return 0;
 }
 
+inline uint8_t paeth_predictor(uint8_t a, uint8_t b, uint8_t c) {
+    int p = a + b - c;
+    int pa = abs(p - a);
+    int pb = abs(p - b);
+    int pc = abs(p - c);
+    if (pa <= pb && pa <= pc) return a;
+    else if (pb <= pc) return b;
+    else return c;
+}
+
+void apply_filters() {
+    const int stride = 160 * 4;
+    int x = 0;
+    int y = 0;
+    uint8_t *pixel_data;
+    uint8_t *current_row;
+    uint8_t *prev_row;
+    uint8_t filter_type, left, above, upper_left;
+    for (y = 0; y < 205; y++) {
+        current_row = decompressed_data + y * (stride + 1);
+        filter_type = current_row[0];
+        pixel_data = &current_row[1];
+        switch (filter_type) {
+            case 0:
+                break;
+            case 1:
+                for (x = 4; x < stride; x++) {
+                    pixel_data[x] += pixel_data[x - 4];
+                }
+                break;
+            case 2:
+                if (y > 0) {
+                    prev_row = decompressed_data + (y - 1) * (stride + 1) + 1;
+                    for (x = 0; x < stride; x++) {
+                        pixel_data[x] += prev_row[x];
+                    }
+                }
+                break;
+            case 3:
+                for (x = 0; x < stride; x++) {
+                    left = (x >= 4) ? pixel_data[x - 4] : 0;
+                    above = (y > 0) ? (decompressed_data + (y - 1) * (stride + 1) + 1)[x] : 0;
+                    pixel_data[x] += (left + above) / 2;
+                }
+                break;
+            case 4:
+                for (x = 0; x < stride; x++) {
+                    left = (x >= 4) ? pixel_data[x - 4] : 0;
+                    above = (y > 0) ? (decompressed_data + (y - 1) * (stride + 1) + 1)[x] : 0;
+                    upper_left = (y > 0 && x >= 4) ? (decompressed_data + (y - 1) * (stride + 1) + 1)[x - 4] : 0;
+                    pixel_data[x] += paeth_predictor(left, above, upper_left);
+                }
+                break;
+        }
+    }
+}
+
+void fill_fb() {
+    int x, y, src_index, dst_index;
+    for (y = 0; y < 205; y++) {
+        for (x = 0; x < 160; x++) {
+            int src_index = y * (160 * 4 + 1) + 1 + x * 4;
+            int dst_index = y * 160 + x;
+            fb[dst_index].r = decompressed_data[src_index];
+            fb[dst_index].g = decompressed_data[src_index + 1];
+            fb[dst_index].b = decompressed_data[src_index + 2];
+            fb[dst_index].a = decompressed_data[src_index + 3];
+        }
+    }
+}
+
 int load_data() {
     size_t buf_size = DECMP_BUF_SIZE;
     int result;
-    
+    FILE *fp;
+    int x, y, pi;
     if (read_chunk(&idat)) {
         debug_serial_printf("Read error: Could not load IDAT\n");
         return -1;
     }
     debug_serial_printf("Read complete: Extracting %d bytes\n", idat.length);
     decompressed_data = malloc(DECMP_BUF_SIZE);
+    // i feel like using uncompress() is kind of cheating.
     result = uncompress(decompressed_data, (uLongf *) &buf_size, idat.data, idat.length);
     if(result != Z_OK) {
         debug_serial_printf("zlib fail with error code: %d\n", result);
         free(decompressed_data);
+        decompressed_data = 0;
         return -1;
     }
     debug_serial_printf("Decompression successful, got %d bytes\n", buf_size);
+    apply_filters();
+    fb = malloc(sizeof(pix_t) * 160 * 205);
+    fill_fb();
+    free(decompressed_data);
+    decompressed_data = 0;
+    fp = fopen("out.ppm", "w+");
+    fprintf(fp, "P3\n160 205\n255\n");
+    for (y = 0; y < 205; y++) {
+        for (x = 0; x < 160; x++) {
+            fprintf(fp, "%d %d %d ", fb[y * 160 + x].r, fb[y * 160 + x].g, fb[y * 160 + x].b);
+        }
+        fprintf(fp, "\n");
+    }
+    fclose(fp);
     return 0;
 }
 
@@ -142,6 +232,9 @@ void unload() {
     }
     if (idat.data) {
         free(idat.data);
+    }
+    if (decompressed_data) {
+        free(decompressed_data);
     }
     return;
 }
