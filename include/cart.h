d014112b86026cbbc8f57267bf8a37987ff563f1
Riley Loo <shaunloo10@gmail.com>
2025-06-10 19:30:59 -0500

    cart: remove the zlib chunk stuff since we're importing zlib now
    Signed-off-by: Riley Loo <shaunloo10@gmail.com>

--- include/cart.h
+++ include/cart.h
@@ -8,13 +8,6 @@ typedef struct {
 } PNG_Chunk; // PNG IDAT
 
 typedef struct {
-    uint8_t cmf;
-    uint8_t extra_flags;
-    uint8_t *data;
-    uint16_t check_value;
-} ZLIB_Chunk;
-
-typedef struct {
     uint8_t r;
     uint8_t g;
     uint8_t b;
@@ -25,4 +18,5 @@ int load_png(const char *);
 int read_chunk();
 int scan_cart();
 int load_data();
+void apply_filters();
 void unload();
